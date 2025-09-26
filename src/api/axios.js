2f7601c10101026165e6dc5e9bd0d3ba6dc3ea45
tpwjd123 <tpwjdskql@gmail.com>
2025-09-26 11:02:57 +0900

    d

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,10 +8,10 @@ const api = axios.create({
 })
 
 
-//Flask API
+//Flask API 챗봇 백엔드 render
 const chatApi = axios.create({
-  // baseURL: 'https://backend-movie-zrez.onrender.com',
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://test-team-i0b9.onrender.com',
+  // baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json'
   }
